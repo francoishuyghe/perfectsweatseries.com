0a0ae3ab2a212de6f718ad44626ff63574a7d7dd
Francois Huyghe <francois@13milliseconds.com>
2020-01-15 16:21:19 -0600

    Header

--- web/app/themes/perfectsweat/resources/assets/scripts/routes/common.js
+++ web/app/themes/perfectsweat/resources/assets/scripts/routes/common.js
@@ -1,6 +1,15 @@
 export default {
   init() {
     // JavaScript to be fired on all pages
+    const banner = $('.banner');
+
+    $(window).scroll(function () { 
+      if ($(window).scrollTop() > 20) { 
+        banner.addClass('scrolled');
+      } else {
+        banner.removeClass('scrolled');
+      }
+    });
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
---
+++ web/app/themes/perfectsweat/resources/views/partials/content-single-episode.blade.php
@@ -0,0 +1,7 @@
+<article {{ post_class() }}>
+  <div class="entry-content">
+    <div class="container">
+      @php the_content() @endphp
+    </div>
+  </div>
+</article>
